40335fc52eb78fa4620b9aa904dc1985c08df0e6
Huy Bui <67461134+huyjs9@users.noreply.github.com>
2022-12-05 18:23:47 +0700

    Feature/cypress (#4)

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,11 @@
+// context('Homepage', () => {
+//   beforeEach(() => {
+//     cy.visit(`http://localhost:8000`)
+//     cy.waitForRouteChange()
+//   })
+
+//   it('has focusable buttons', () => {
+//     cy.findByText('click me').focus()
+//     cy.focused().should('have.text', 'click me')
+//   })
+// })
---
+++ cypress/plugin/index.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+// eslint-disable-next-line no-unused-vars
+require('dotenv').config()
+module.exports = (on, config) => {
+  config.env.SITEMAP_TEST_URL = process.env.SITEMAP_TEST_URL
+  on('before:browser:launch', (browser, launchOptions) => {
+    if (browser.name === 'chrome' && browser.isHeadless) {
+      launchOptions.args.push('--disable-gpu')
+      return launchOptions
+    }
+  })
+  return config
+}
